1f94643c5c805538518603037b5dbe5b3f7f5ea8
jaymesFW <89203454+jaymesFW@users.noreply.github.com>
2021-08-19 16:03:47 +0100

    Update code.py

--- code.py
+++ code.py
@@ -177,6 +177,7 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['left'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
+ax.axes.xaxis.set_visible(False)
 
 for i, v in enumerate(databar[variable]):
     if v >= 0:
