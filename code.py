bb774b5ca00b9aa552376fcaa4d2e4fdb765fe62
jaymesFW <89203454+jaymesFW@users.noreply.github.com>
2021-09-01 09:04:55 +0100

    Update code.py

--- code.py
+++ code.py
@@ -96,9 +96,9 @@ mins_choice = st.sidebar.number_input(
 
 data = data[data['90s'] > mins_choice]
 
-teamcolor=st.sidebar.color_picker('Primary Team Colour')
+teamcolor=st.sidebar.color_picker('Primary Team Colour','#EF0107')
 
-teamcolor2=st.sidebar.color_picker('Secondary Team Colour')
+teamcolor2=st.sidebar.color_picker('Secondary Team Colour','#FFFFFF')
 
 #teamcolor = st.sidebar.text_input ('Enter team hex colour (include #)',
  #                                   value="red")
